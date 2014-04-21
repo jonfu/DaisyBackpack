f9130f6233ce1078a4c40cff1f964f34e81e84a6
jonfu <mba.msee@gmail.com>
2014-04-20 23:02:40 -0700

    initial working version checkin
    freezes after a minute

---
+++ DaisyBackpack.ino
@@ -0,0 +1,106 @@
+#include <Adafruit_NeoPixel.h>
+ 
+#define PIN 4
+#define STRIPSIZE 24
+#define PETALSIZE 8
+#define MODESIZE 4
+#define trigPin 13
+#define echoPin 12
+#define distDiffThreshod 25
+
+const int petal[] = { 5, 6, 8, 9, 10, 11, 2, 3 };
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+// NEO_KHZ800 800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+// NEO_KHZ400 400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+// NEO_GRB Pixels are wired for GRB bitstream (most NeoPixel products)
+// NEO_RGB Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPSIZE, PIN, NEO_GRB + NEO_KHZ800);
+long distance = 72;
+long distDiff = 0;
+int mode = 0;
+int wait = 144;
+int petalIndex = PETALSIZE-1;
+boolean pixelRunning = false;
+
+void setup() {
+  //Serial.begin (9600);
+  
+  for (int i=0; i<PETALSIZE; i++) {
+    pinMode(petal[i], OUTPUT);
+  }  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  strip.begin();
+  strip.setBrightness(5); // Lower brightness and save eyeballs!
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  if (pixelRunning == false) {
+     switch (mode) {
+       case 0:
+       wiper();
+       break;
+       
+       case 1:
+       rainbow();
+       break;
+       
+       case 2:
+       rainbowCycle();
+       break;
+       
+       case 3:
+       colorWave();
+       break;
+     } 
+  }
+}
+
+
+
+void calculateDistance() {
+ 
+  long duration, dist;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  dist = (duration/2) / 29.1;
+  
+  if (dist > 0 && dist < 200){
+    distDiff = abs(distance - dist);
+    if (distDiff > distDiffThreshod) {
+      changeMode();
+    }
+    distance = dist;
+    wait = distance * 2;
+  }
+  else {
+    //Serial.println("Out of range");
+  }
+  
+}
+
+void changeMode() {
+  if (++mode >= MODESIZE) {
+    mode = 0; 
+  }
+}
+
+void updatePetal() {
+  
+  digitalWrite(petal[petalIndex], LOW);
+  
+  if (++petalIndex == PETALSIZE) {
+     petalIndex = 0; 
+  }
+  
+  digitalWrite(petal[petalIndex], HIGH);
+  
+}
---
+++ NeoPixel.ino
@@ -0,0 +1,209 @@
+byte petalCounter = 0;
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    if (mode != 0) {
+       return; 
+    }
+    strip.setPixelColor(i, c);
+    strip.show();
+    if (++petalCounter == 3) {
+      petalCounter = 0;
+      updatePetal();
+    }
+    delay(wait);
+    calculateDistance();
+  }
+}
+
+void colorWipeReverse(uint32_t c) {
+  int pos = strip.numPixels();
+  while (--pos >= 0) {
+    if (mode != 0) {
+       return; 
+    }
+    strip.setPixelColor(pos, c);
+    strip.show();
+    if (++petalCounter == 3) {
+      petalCounter = 0;
+      updatePetal();
+    }
+    delay(wait);
+    calculateDistance();
+  }
+}
+
+void wiper() {
+  
+  pixelRunning = true;
+  
+  while (true) {
+  
+    colorWipe(strip.Color(0, 0, 64)); // Blue
+    if (mode != 0) {
+       pixelRunning = false;
+       return; 
+    }
+    colorWipeReverse(strip.Color(64,0,64)); 
+    if (mode != 0) {
+       pixelRunning = false;
+       return; 
+    }
+    colorWipe(strip.Color(64,64,0)); 
+    if (mode != 0) {
+       pixelRunning = false;
+       return; 
+    }
+    colorWipeReverse(strip.Color(0, 64, 64));
+    if (mode != 0) {
+       pixelRunning = false;
+       return; 
+    }
+    
+    colorWipe(strip.Color(0, 0, 255)); // Blue
+    if (mode != 0) {
+       pixelRunning = false;
+       return; 
+    }
+    colorWipeReverse(strip.Color(255,0,255)); 
+    if (mode != 0) {
+       pixelRunning = false;
+       return; 
+    }
+    colorWipe(strip.Color(255,255,0)); 
+    if (mode != 0) {
+       pixelRunning = false;
+       return; 
+    }
+    colorWipeReverse(strip.Color(0, 255, 255));
+    if (mode != 0) {
+       pixelRunning = false;
+       return; 
+    }
+  
+  }
+  
+}
+ 
+void rainbow() {
+  uint16_t i, j;
+  
+  pixelRunning = true;
+  for(j=0; j<256; j++) {
+    if (mode != 1) {
+       pixelRunning = false;
+       return; 
+    }
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    if (++petalCounter == 3) {
+      petalCounter = 0;
+      updatePetal();
+    }
+    delay(wait);
+    calculateDistance();
+  }
+}
+ 
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle() {
+  uint16_t i, j;
+  
+  pixelRunning = true;
+   
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    if (mode != 2) {
+       pixelRunning = false;
+       return; 
+    }
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    if (++petalCounter == 3) {
+        petalCounter = 0;
+        updatePetal();
+    }
+    delay(wait);
+    calculateDistance();
+  }
+}
+ 
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+  uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+ 
+/**
+* ^ ^ ^
+* ~~~~~ ColorWave ~~~~~
+* V V V
+*/
+void colorWave() {
+  int i, j, stripsize, cycle;
+  float ang, rsin, gsin, bsin, offset;
+   
+  static int tick = 0;
+  pixelRunning = true;
+  stripsize = strip.numPixels();
+  cycle = stripsize * 25; // times around the circle...
+   
+  while (++tick % cycle) {
+    if (mode != 3) {
+       pixelRunning = false;
+       return; 
+    }
+    offset = map2PI(tick);
+     
+    for (i = 0; i < stripsize; i++) {
+      ang = map2PI(i) - offset;
+      rsin = sin(ang);
+      gsin = sin(2.0 * ang / 3.0 + map2PI(int(stripsize/6)));
+      bsin = sin(4.0 * ang / 5.0 + map2PI(int(stripsize/3)));
+      strip.setPixelColor(i, strip.Color(trigScale(rsin), trigScale(gsin), trigScale(bsin)));
+    }
+     
+    strip.show();
+    if (++petalCounter == 3) {
+        petalCounter = 0;
+        updatePetal();
+    }
+    delay(wait);
+    calculateDistance();
+  }
+ 
+}
+ 
+/**
+* Scale a value returned from a trig function to a byte value.
+* [-1, +1] -> [0, 254]
+* Note that we ignore the possible value of 255, for efficiency,
+* and because nobody will be able to differentiate between the
+* brightness levels of 254 and 255.
+*/
+byte trigScale(float val) {
+  val += 1.0; // move range to [0.0, 2.0]
+  val *= 127.0; // move range to [0.0, 254.0]
+   
+  return int(val) & 255;
+}
+ 
+/**
+* Map an integer so that [0, striplength] -> [0, 2PI]
+*/
+float map2PI(int i) {
+  return PI*2.0*float(i) / float(strip.numPixels());
+}
+
